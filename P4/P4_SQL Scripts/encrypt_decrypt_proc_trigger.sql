6ae0abb6a9975038da2df7a4e48918f3e4dc6c5d
Amey Satwe <amey10051999@gmail.com>
2023-11-29 20:10:57 -0500

    Update UDF to compute columns for table

--- P4/P4_SQL Scripts/encrypt_decrypt_proc_trigger.sql
+++ P4/P4_SQL Scripts/encrypt_decrypt_proc_trigger.sql
@@ -14,17 +14,3 @@ BEGIN
     CLOSE SYMMETRIC KEY MySymmetricKey;
 END;
 
--- -- CREATE TRIGGER EncryptPaymentInfo
--- -- ON Customer
--- -- AFTER INSERT
--- -- AS
--- -- BEGIN
--- --     OPEN SYMMETRIC KEY MySymmetricKey DECRYPTION BY CERTIFICATE MyCertificate;
-
--- --     UPDATE Customer
--- --     SET EncryptedPaymentInfo = ENCRYPTBYKEY(KEY_GUID('MySymmetricKey'), i.EncryptedPaymentInfo)
--- --     FROM Customer c
--- --     INNER JOIN inserted i ON c.CustomerID = i.CustomerID;
-
--- --     CLOSE SYMMETRIC KEY MySymmetricKey;
--- -- END;
